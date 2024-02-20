d30ee70a82828b18222e7b6f8902186fa9fa267e
SerhiiNikulin <sergey.nikulin99@gmail.com>
2024-02-21 00:10:00 +0200

    Reflections from all sides of bricks(corrections)

--- BoardBall_Game/Level.h
+++ BoardBall_Game/Level.h
@@ -21,8 +21,10 @@ public:
 private:
 	void Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type);
 	void Set_Brick_Letter_Colors(bool is_switch_color, HPEN &front_pen, HBRUSH &front_brush, HPEN &back_pen, HBRUSH &back_brush);
+	bool Is_Horizontal_Hit_First(double next_x_pos, double next_y_pos);
+	bool Check_Horizontal_Hit(double next_x_pos, double next_y_pos, int level_x, int level_y, ABall *ball);
+	bool Check_Vertical_Hit(double next_x_pos, double next_y_pos, int level_x, int level_y, ABall *ball);
 	bool Hit_Circle_On_Line(double next_x_pos, double y, double radius, double min_x, double max_x);
-
 
 	HPEN Brick_Blue_Pen;
 	HBRUSH Brick_Blue_Brush;
@@ -30,6 +32,9 @@ private:
 	HBRUSH Brick_Red_Brush;
 	HPEN Letter_Pen;
 	RECT Level_Rect;
+
+	double Current_Brick_Left_X, Current_Brick_Right_X;
+	double Current_Brick_Top_Y, Current_Brick_Low_Y;
 
 	static const int Level_Height = 14;
 	static const int Level_Width = 12;
